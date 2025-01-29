b9ff65f0d65a95efa17f2de0c40bfa9f1b886588
NikonoA <alena.nikonorova12@gmail.com>
2025-01-29 23:50:23 +0300

    create branch

---
+++ lesson_02/lesson_2_task_2.py
@@ -0,0 +1,11 @@
+def is_year_leap(year):
+    if year % 4 == 0:
+        return ("True")
+    else:
+        return ("False")
+
+
+y = int(input("Введите год: "))
+
+result = is_year_leap(y)
+print(f"год {y}: {result}")
