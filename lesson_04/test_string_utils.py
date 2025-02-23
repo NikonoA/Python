ced1c608f7de813906f0e9516f19b07704022695
NikonoA <alena.nikonorova12@gmail.com>
2025-02-24 00:56:08 +0300

    changes for homework

--- lesson_04/test_string_utils.py
+++ lesson_04/test_string_utils.py
@@ -3,46 +3,72 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
+
 @pytest.mark.positive
-@pytest.mark.parametrize("input_str, expected", [("alex", "Alex"), ("hello world", "Hello world")])
+@pytest.mark.parametrize("input_str, expected", [
+    ("alex", "Alex"), ("hello world", "Hello world")])
 def test_capitalize_positive(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
+
 @pytest.mark.positive
-@pytest.mark.parametrize("input_str, expected", [(" Alex", "Alex"), (" 12345", "12345"), (" !!!!####", "!!!!####"), (" hello world", "hello world")])
+@pytest.mark.parametrize("input_str, expected", [
+    (" Alex", "Alex"), (" 12345", "12345"),
+    (" !!!!####", "!!!!####"), (" hello world", "hello world")])
 def test_trim_positive(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
+
 @pytest.mark.positive
-@pytest.mark.parametrize("word, symbol, answer", [("Hello", "H", True), ("Nice to meet you", "o", True), ("123456", "5", True), ("Hi!", "!", True), ("Cool", "c", True)])
+@pytest.mark.parametrize("word, symbol, answer", [
+    ("Hello", "H", True), ("Nice to meet you", "o", True),
+    ("123456", "5", True), ("Hi!", "!", True),
+    ("Cool", "c", True)])
 def test_contains_positive(word, symbol, answer):
     ans = string_utils.contains(word, symbol)
     assert ans == answer
 
+
 @pytest.mark.positive
-@pytest.mark.parametrize("word, symbol, string", [("Line", "i", "Lne"), ("How are you", "e", "How ar you"), ("547", "4", "57"), ("Awesome!!!", "!", "Awesome"), ("Range", "ange", "R"), ("Moon", "O", "Mn")])
+@pytest.mark.parametrize("word, symbol, string", [
+    ("Line", "i", "Lne"), ("How are you", "e", "How ar you"),
+    ("547", "4", "57"), ("Awesome!!!", "!", "Awesome"),
+    ("Range", "ange", "R"), ("Moon", "O", "Mn")])
 def test_delete_symbol_positive(word, symbol, string):
     ans = string_utils.delete_symbol(word, symbol)
     assert ans == string
 
+
 @pytest.mark.negative
-@pytest.mark.parametrize("input_str, expected", [("123abc", "123abc"), ("!!", "!!"), ("", ""), ("   ", "   ")])
+@pytest.mark.parametrize("input_str, expected", [
+    ("123abc", "123abc"), ("!!", "!!"), ("", ""),
+    ("   ", "   ")])
 def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
+
 @pytest.mark.negative
-@pytest.mark.parametrize("input_str, expected", [("", ""), ("  ", " ")])
+@pytest.mark.parametrize("input_str, expected", [
+    ("", ""), ("  ", " ")])
 def test_trim_negative(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
+
 @pytest.mark.negative
-@pytest.mark.parametrize("word, symbol, answer", [(" ", " ", True), ("", "", True), ("cool", "с", False)]) #в последнем примере изменён язык
+@pytest.mark.parametrize("word, symbol, answer", [
+    (" ", " ", True), ("", "", True),
+    ("cool", "с", False)])  # в последнем примере изменён язык
 def test_contains_negative(word, symbol, answer):
     ans = string_utils.contains(word, symbol)
     assert ans == answer
 
+
 @pytest.mark.negative
-@pytest.mark.parametrize("word, symbol, string", [("Line ", " ", "Line"), ("", "", ""), ("  ", " ", ""), ("Hello World", " ", "HelloWorld")])
+@pytest.mark.parametrize("word, symbol, string", [
+    ("Line ", " ", "Line"),
+    ("", "", ""), ("  ", " ", ""),
+    ("Hello World", " ", "HelloWorld")])
 def test_delete_symbol_negative(word, symbol, string):
+
     ans = string_utils.delete_symbol(word, symbol)
     assert ans == string
